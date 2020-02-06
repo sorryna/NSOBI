c922dbe8560ae20b3ea18cdb9d69ab39e54f825a
sikarin <so.sikarin@perfenterprise.com>
2020-02-06 16:03:41 +0700

    UI for BI

---
+++ NSOBI/src/models/SubWMI.ts
@@ -0,0 +1,81 @@
+export interface SubWMI {
+    name: string,
+    main: string,
+    code: string,
+    graph: string
+}
+
+export const Sub: SubWMI[] = [
+    {
+        name: "3.1 ด้านเกษตรกรรม",
+        main: "M3",
+        code: "S31",
+        graph: "Graph31"
+    },
+    {
+        name: "3.2 ด้านอุตสาหกรรม",
+        main: "M3",
+        code: "S32",
+        graph: "Graph32"
+    },
+    {
+        name: "3.3 ด้านบริการ",
+        main: "M3",
+        code: "S33",
+        graph: "Graph33"
+    },
+    {
+        name: "6.1 ด้านน้ำท่วม",
+        main: "M6",
+        code: "S61",
+        graph: "Graph61"
+    },
+    {
+        name: "6.2 ด้านภัยแล้ง",
+        main: "M6",
+        code: "S62",
+        graph: "Graph62"
+    },
+    {
+        name: "8.1 ด้านแผนการจัดการลุ่มน้ำ",
+        main: "M8",
+        code: "S81",
+        graph: "Graph81"
+    },
+    {
+        name: "8.2 ด้านการมีส่วนร่วมในการจัดการน้ำ",
+        main: "M8",
+        code: "S82",
+        graph: "Graph82"
+    },
+    {
+        name: "8.3 ด้านองค์กรจัดการน้ำ",
+        main: "M8",
+        code: "S83",
+        graph: "Graph83"
+    },
+    {
+        name: "8.4 ด้านศักยภาพในการพัฒนา",
+        main: "M8",
+        code: "S84",
+        graph: "Graph84"
+    },
+    {
+        name: "8.5 ด้านการรักษาสภาพทางน้ำเพื่อการขนส่งทางน้ำ",
+        main: "M8",
+        code: "S85",
+        graph: "Graph85"
+    },
+    {
+        name: "8.6 ด้านการติดตามตรวจสอบ",
+        main: "M8",
+        code: "S86",
+        graph: "Graph86"
+    },
+    {
+        name: "8.7 ด้านการจัดสรรน้ำ",
+        main: "M6",
+        code: "S87",
+        graph: "Graph87"
+    }
+]
---
+++ NSOBI/src/models/WMI.ts
@@ -0,0 +1,48 @@
+export interface MainWMI {
+    name: string,
+    code: string,
+    graph: string
+}
+
+export const Main: MainWMI[] = [
+    {
+        name: "1. มิติต้นทุนทรัพยากรน้ำ",
+        code: "M1",
+        graph: "Graph1"
+    },
+    {
+        name: "2. มิติการจัดการน้ำเพื่อการอุปโภค บริโภค",
+        code: "M2",
+        graph: "Graph2"
+    },
+    {
+        name: "3. มิติความมั่นคงของน้ำเพื่อการพัฒนา",
+        code: "M3",
+        graph: "Graph3"
+    },
+    {
+        name: "4. มิติความสมดุลของน้ำต้นทุนและการใช้น้ำ",
+        code: "M4",
+        graph: "Graph4"
+    },
+    {
+        name: "5. มิติการจัดการคุณภาพน้ำและสิ่งแวดล้อม",
+        code: "M5",
+        graph: "Graph5"
+    },
+    {
+        name: "6. มิติการจัดการภัยพิบัติที่เกิดจากน้ำ",
+        code: "M6",
+        graph: "Graph6"
+    },
+    {
+        name: "7. มิติการจัดการและอนุรักษ์ป่าต้นน้ำ",
+        code: "M7",
+        graph: "Graph7"
+    },
+    {
+        name: "8. มิติการบริหารจัดการทรัพยากรน้ำ",
+        code: "M8",
+        graph: "Graph8"
+    },
+]
