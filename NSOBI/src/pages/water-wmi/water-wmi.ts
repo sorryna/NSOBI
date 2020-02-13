8b902d4bf605823ec0b57a0b911e4a21e7fb479e
thaninpong <thaninpong@perfenterprise.com>
2020-02-13 11:24:52 +0700

    Add 8miti Home

--- NSOBI/src/pages/water-wmi/water-wmi.ts
+++ NSOBI/src/pages/water-wmi/water-wmi.ts
@@ -14,7 +14,7 @@ export class WaterWmiPage {
   public graph2: SafeResourceUrl;
   public Area: any;
   public areaGraph: AreaGraph[] = AGraph;
-  firstGraph ="";
+  firstGraph ="https://app.powerbi.com/reportEmbed?reportId=36b6765d-6fc7-4fb9-bc76-72c07a7dc287&autoAuth=true&ctid=93793cef-3400-4bdb-81f4-925ccb3a6924&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWVhc3QtYXNpYS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D";
   firstGraph2 ="https://app.powerbi.com/reportEmbed?reportId=91038c2e-0282-417f-bdb6-296947d52362&autoAuth=true&ctid=93793cef-3400-4bdb-81f4-925ccb3a6924&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWVhc3QtYXNpYS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D";
   constructor(public navCtrl: NavController, public navParams: NavParams, public sanitizer: DomSanitizer) {
     console.log(this.Area);
