60e73d0856d862120d35b636359e7821af6d319c
sikarin <so.sikarin@perfenterprise.com>
2020-03-12 14:14:03 +0700

    change UI

--- NSOBI/src/pages/downloaddata/downloaddata.ts
+++ NSOBI/src/pages/downloaddata/downloaddata.ts
@@ -1,22 +1,20 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-/**
- * Generated class for the DownloaddataPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
   selector: 'page-downloaddata',
   templateUrl: 'downloaddata.html',
 })
+
 export class DownloaddataPage {
+
   Area: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.Area = "Reg";
   }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad DownloaddataPage');
   }
--- NSOBI/src/pages/home/home.ts
+++ NSOBI/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public regAreaGraph: SafeResourceUrl;
+  allInThailand = "https://app.powerbi.com/reportEmbed?reportId=995b096f-aea3-4795-b821-f8a29a3c82bb&autoAuth=true&ctid=93793cef-3400-4bdb-81f4-925ccb3a6924&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWVhc3QtYXNpYS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D";
 
+
+  constructor(public navCtrl: NavController, public sanitizer: DomSanitizer) {
+    this.regAreaGraph = this.sanitizer.bypassSecurityTrustResourceUrl(this.allInThailand);
   }
 
   goWaterIndexPage(){
