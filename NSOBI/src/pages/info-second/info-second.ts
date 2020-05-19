90c649bfb9994051721abe008f10a642036e4b58
sikarin <so.sikarin@perfenterprise.com>
2020-05-19 11:56:52 +0700

    change header

--- NSOBI/src/pages/info-second/info-second.ts
+++ NSOBI/src/pages/info-second/info-second.ts
@@ -23,36 +23,5 @@ export class InfoSecondPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad InfoSecondPage');
   }
-  
-  goToPage(){
-    console.log(this.WMImenu);
-    if (this.WMImenu == '1') {
-      this.navCtrl.setRoot("WaterIndexPage")
-    }
-    if (this.WMImenu == '2') {
-      this.navCtrl.setRoot("WaterWmiPage")
-    }
-    if (this.WMImenu == '3') {
-      this.navCtrl.setRoot("WmiMenuPage")
-    }
-    if (this.WMImenu == '4') {
-      this.navCtrl.setRoot("DownloaddataPage")
-    }
-    if (this.WMImenu == '5') {
-      this.navCtrl.setRoot("InfoFirstPage")
-    }
-    if (this.WMImenu == '6') {
-      this.navCtrl.setRoot("InfoSecondPage")
-    }
-    if (this.WMImenu == '7') {
-      this.navCtrl.setRoot("InfoThirdPage")
-    }
-    if (this.WMImenu == '8') {
-      this.navCtrl.setRoot("InfofourthPage")
-    }
-  }
-
-  goHomePage(){
-      this.navCtrl.setRoot(HomePage)
-  }
+ 
 }
--- NSOBI/src/pages/water-wmi/water-wmi.ts
+++ NSOBI/src/pages/water-wmi/water-wmi.ts
@@ -23,35 +23,4 @@ export class WaterWmiPage {
     console.log('ionViewDidLoad WaterWmiPage');
   }
 
-  goToPage() {
-    console.log(this.WMImenu);
-    if (this.WMImenu == '1') {
-      this.navCtrl.setRoot("WaterIndexPage")
-    }
-    if (this.WMImenu == '2') {
-      this.navCtrl.setRoot("WaterWmiPage")
-    }
-    if (this.WMImenu == '3') {
-      this.navCtrl.setRoot("WmiMenuPage")
-    }
-    if (this.WMImenu == '4') {
-      this.navCtrl.setRoot("DownloaddataPage")
-    }
-    if (this.WMImenu == '5') {
-      this.navCtrl.setRoot("InfoFirstPage")
-    }
-    if (this.WMImenu == '6') {
-      this.navCtrl.setRoot("InfoSecondPage")
-    }
-    if (this.WMImenu == '7') {
-      this.navCtrl.setRoot("InfoThirdPage")
-    }
-    if (this.WMImenu == '8') {
-      this.navCtrl.setRoot("InfofourthPage")
-    }
-  }
-
-  goHomePage() {
-    this.navCtrl.setRoot(HomePage)
-  }
 }
